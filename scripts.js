e421fe09c422e8ef187912f0a4a1477189022d7d
A-Warsame <aawarsame12@gmail.com>
2024-08-22 11:26:29 +0100

    Created function for basic math operators

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const add = function(a,b) {
+    return a + b;
+      
+  };
+  
+const subtract = function(a,b) {
+  return a - b;
+
+};
+
+const sum = function(arr) {
+  return arr.reduce((a, b) => (a + b),0)
+    
+};
+
+const multiply = function(arr) {
+  return arr.reduce((a, b) => (a * b))
+
+};
+
+const divide = function(a,b) {
+  return a / b;
+};
