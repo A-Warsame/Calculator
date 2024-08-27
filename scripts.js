353c061c0ed030a027a69523ba17fb9677cef589
A-Warsame <aawarsame12@gmail.com>
2024-08-27 14:42:06 +0100

    Added a clear button and resets calculator to 0

--- scripts.js
+++ scripts.js
@@ -1,30 +1,28 @@
-const add = (a,b) => a + b;
+const add = (a, b) => a + b;
   
-const subtract = (a,b) => a - b;
+const subtract = (a, b) => a - b;
 
-const multiply  = (a,b) => a * b;
+const multiply = (a, b) => a * b;
 
-const divide  = (a,b) => a/b;
+const divide = (a, b) => a / b;
 
 const operators = ["+", "-", "*", "/"];
 
 function operate(num1, num2, operator) {
     switch (operator) {
         case "+":
-        return add(num1, num2);
+            return add(num1, num2);
         case "-":
-        return subtract(num1, num2);
+            return subtract(num1, num2);
         case "*":
-        return multiply(num1, num2);
+            return multiply(num1, num2);
         case "/":
-        return divide(num1, num2);
+            return divide(num1, num2);
     }
 };
 
-const displayValue = "";
 let firstNum = "";
 let secondNum = "";
-const container = document.querySelector(".container");
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
 
@@ -33,8 +31,13 @@ let currentOperator = ""; // Track the current operator
 buttons.forEach((button) => {
   button.addEventListener("click", e => {
     const value = e.target.innerText;
-    
-    if (operators.includes(value)) { 
+
+    if (value === "C") {
+      firstNum = "";
+      secondNum = "";
+      currentOperator = "";
+      display.textContent = "0";
+    } else if (operators.includes(value)) { 
       currentOperator = value; // Set the operator
     } else if (value === "=") {
       // Perform the operation
